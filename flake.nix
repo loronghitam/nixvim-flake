209e3a19085c5226194b5810b5b02c6147631fab
loronghitam <sutio.mudiarno@gmail.com>
2024-04-06 03:03:43 +0700

    first commit

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "A nixvim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixvim.url = "github:nix-community/nixvim/nixos-23.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixvim,
+    flake-parts,
+    ...
+  } @ inputs: let
+    config = import ./config;
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        nixvimLib = nixvim.lib.${system};
+        nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
+          inherit pkgs;
+          module = config;
+        };
+      in {
+        checks = {
+          default = nixvimLib.check.mkTestDerivationFromNvim {
+            inherit nvim;
+            name = "A nixvim configuration";
+          };
+        };
+
+        packages = {
+          default = nvim;
+        };
+
+        devShells.default = pkgs.mkShellNoCC {
+          shellHook = ''
+            echo Welcome to a Neovim dev environment powered by Nixvim -- https://github.com/nix-community/nixvim
+            PS1="Nixvim: \\w \$ "
+            alias vim='nvim'
+          '';
+          packages = with pkgs; [
+            nvim
+          ];
+        };
+      };
+    };
+}
